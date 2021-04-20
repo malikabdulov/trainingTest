96af468f74b6a99fa9490b6fbe757eac9e7eaa9d
malik <malik.abdulov@gmail.com>
2021-04-20 13:44:57 +0600

    bot runner .bat

---
+++ run_main.bat
@@ -0,0 +1,2 @@
+python run_main.py
+pause
---
+++ run_main.py
@@ -0,0 +1,7 @@
+from subprocess import Popen
+
+filename = 'main.py'
+while True:
+    print("\nStarting " + filename)
+    p = Popen("python " + filename, shell=True)
+    p.wait()

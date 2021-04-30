5f81501061893cf04ce0a9ac818e643babc338ed
malik <malik.abdulov@gmail.com>
2021-04-30 17:55:34 +0600

    added: 1) Export and send report to admins 2) Test timer 20 min 3) in config file added admins (list) and minutes_to_test (int)

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import config
 import mysql.connector
+import xlsxwriter
 from mysql.connector import Error
 
 
@@ -173,6 +174,70 @@ def get_test_results(telegram_id, test_id):
         close_db_connection(cnx, cursor)
 
 
+def get_report(test_id):
+    cnx = create_db_connection()
+    cursor = cnx.cursor()
+    query = (
+        "SELECT DATE(ta.created_at) date, u.student_name "
+        ", SUM(CASE WHEN tqv.is_right_variant = 'Y' THEN 1 ELSE 0 END) right_answers "
+        "FROM test_answers ta "
+        "JOIN users u ON u.id = ta.student_id "
+        "JOIN test_question_variants tqv ON ta.variant_id = tqv.id "
+        "JOIN test_questions tq ON tq.id = tqv.test_question_id "
+        f"AND tq.test_id = {test_id} "
+        "GROUP BY 1, 2 "
+    )
+    try:
+        cursor.execute(query)
+        header = [row[0] for row in cursor.description]
+        rows = cursor.fetchall()
+        return header, rows
+    except Error as e:
+        print(cursor.statement())
+        print(f"def set_answer:::Error '{e}' occurred")
+        return None
+    finally:
+        close_db_connection(cnx, cursor)
+
+
+def export_report(test_id):
+    # Create an new Excel file and add a worksheet.
+    workbook = xlsxwriter.Workbook('Report.xlsx')
+    worksheet = workbook.add_worksheet('REPORT')
+
+    # Create style for cells
+    header_cell_format = workbook.add_format({'bold': True, 'border': True, 'bg_color': 'yellow'})
+    body_cell_format = workbook.add_format({'border': True})
+    body_date_format = workbook.add_format({'border': True, 'num_format': 'd mmm yyyy'})
+
+    header, rows = get_report(test_id)
+
+    row_index = 0
+    column_index = 0
+
+    for column_name in header:
+        worksheet.write(row_index, column_index, column_name, header_cell_format)
+        column_index += 1
+
+    row_index += 1
+    for row in rows:
+        column_index = 0
+        for column in row:
+            if column_index == 0:
+                worksheet.write(row_index, column_index, column, body_date_format)
+            else:
+                worksheet.write(row_index, column_index, column, body_cell_format)
+            column_index += 1
+        row_index += 1
+
+    worksheet.set_column('A:C', 20)
+
+    print(str(row_index) + ' rows written successfully to ' + workbook.filename)
+
+    # Closing workbook
+    workbook.close()
+
+
 def already_taken(telegram_id, test_id):
     cnx = create_db_connection()
     cursor = cnx.cursor()
@@ -198,7 +263,39 @@ def already_taken(telegram_id, test_id):
         close_db_connection(cnx, cursor)
 
 
+def check_first_answer_time(test_id, telegram_id):
+    cnx = create_db_connection()
+    cursor = cnx.cursor()
+    query = (
+        "SELECT ta.created_at "
+        "FROM test_answers ta "
+        "JOIN users u ON u.id = ta.student_id "
+        "JOIN test_question_variants tqv ON tqv.id = ta.variant_id "
+        "JOIN test_questions tq ON tq.id = tqv.test_question_id "
+        "WHERE DATE(ta.created_at) = CURDATE() "
+        f"AND tq.test_id = {test_id} "
+        f"AND u.telegram_id = {telegram_id} "
+        "ORDER BY ta.variant_id "
+        "LIMIT 1 "
+    )
+    try:
+        cursor.execute(query)
+        output = cursor.fetchone()
+        try:
+            return output[0]
+        except:
+            return None
+    except Error as e:
+        print(cursor.statement())
+        print(f"def set_answer:::Error '{e}' occurred")
+        return None
+    finally:
+        close_db_connection(cnx, cursor)
+
+
 if __name__ == '__main__':
-    r = already_taken(413974882, 1)
-    print(r[0])
-    print(r)
+    import datetime
+    r = check_first_answer_time(1, 413974882)
+    diff = datetime.datetime.now() - r
+    print(diff.seconds/60)
+
--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeybo
 import database as sql
 import config
 import re
-from datetime import date, timedelta
+import time
+from datetime import datetime
 
 bot = telebot.TeleBot(token=config.potatobot['token'], parse_mode='Markdown')
 
@@ -22,9 +23,12 @@ def start_message(message):
 
 @bot.message_handler(commands=['help'])
 def help_message(message):
-    key_test = InlineKeyboardButton(text='Start test', callback_data='start_test')
+    key_test = InlineKeyboardButton(text='Начать тест', callback_data='start_test')
     markup_inline = InlineKeyboardMarkup()
     markup_inline.add(key_test)
+    if message.from_user.id in config.admins:
+        key_report = InlineKeyboardButton(text='Получить статистику', callback_data='get_report')
+        markup_inline.add(key_report)
     text = f'Available commands\n/start\n/help'
     chat_id = message.chat.id
     bot.send_message(chat_id=chat_id, text=text, reply_markup=markup_inline)
@@ -52,6 +56,16 @@ def get_name(call):
         text = f'Напиши свое имя.'
         msg = bot.edit_message_text(message_id=msg_id, chat_id=chat_id, text=text)
         bot.register_next_step_handler(msg, start_test)
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'get_report')
+def send_report(call):
+    print('Callback:', call.data)
+    chat_id = call.message.chat.id
+    sql.export_report(1)
+    time.sleep(1.5)
+    f = open('Report.xlsx', 'rb')
+    bot.send_document(chat_id=chat_id, data=f)
 
 
 @bot.callback_query_handler(func=lambda call: re.match(pattern='v_id=\d+row_num=\d+test_id=\d+', string=call.data))
@@ -83,31 +97,51 @@ def start_test(message):
 
 
 def send_questions(message, test_id, row_num=0):
-    chat_id = message.chat.id
     telegram_id = message.chat.id
-    question = sql.get_question(test_id=test_id, row_num=row_num)
-    if question:
-        q_id = question[0]
-        q_text = question[1]
-        variants = sql.get_variants(question_id=q_id)
-        markup_inline = InlineKeyboardMarkup()
-        text = q_text + '\n'
-        for variant in variants:
-            v_id = variant[0]
-            v_text = variant[1]
-            text = text + f'\n• {v_text}'
-            markup_inline.add(InlineKeyboardButton(text=f'{v_text}',
-                                                   callback_data=f'v_id={v_id}row_num={row_num}test_id={test_id}'))
-        bot.send_message(chat_id=chat_id, text=text, reply_markup=markup_inline)
-    else:
-        bot.send_message(chat_id=chat_id, text='Тест завершен, глянем на твои результаты.')
-        bot.send_sticker(chat_id=chat_id, data='CAACAgIAAxkBAAECMg5geq9IrghFjdKmFmEbjTigXmmGeAACMAADNraOCPwLBo-827_NHwQ')
+    chat_id = message.chat.id
+    time_end = False
+    if row_num != 0:
+        start_time = sql.check_first_answer_time(test_id, telegram_id)
+        diff = datetime.now() - start_time
+        diff_minutes = diff.seconds/60
+        if diff_minutes >= config.minutes_to_test:
+            time_end = True
+    if time_end:
+        bot.send_message(chat_id=chat_id, text='Время закончилось, глянем на твои результаты.')
+        bot.send_sticker(chat_id=chat_id,
+                         data='CAACAgIAAxkBAAECMg5geq9IrghFjdKmFmEbjTigXmmGeAACMAADNraOCPwLBo-827_NHwQ')
         results = sql.get_test_results(telegram_id, test_id)
         correct = results[0]
         incorrect = results[1]
         num_questions = results[2]
         text = f'Correct answers - {correct}\nIncorrect answers - {incorrect}\nNumber of questions - {num_questions}'
         bot.send_message(chat_id=chat_id, text=text)
+    else:
+        chat_id = message.chat.id
+        telegram_id = message.chat.id
+        question = sql.get_question(test_id=test_id, row_num=row_num)
+        if question:
+            q_id = question[0]
+            q_text = question[1]
+            variants = sql.get_variants(question_id=q_id)
+            markup_inline = InlineKeyboardMarkup()
+            text = q_text + '\n'
+            for variant in variants:
+                v_id = variant[0]
+                v_text = variant[1]
+                text = text + f'\n• {v_text}'
+                markup_inline.add(InlineKeyboardButton(text=f'{v_text}',
+                                                       callback_data=f'v_id={v_id}row_num={row_num}test_id={test_id}'))
+            bot.send_message(chat_id=chat_id, text=text, reply_markup=markup_inline)
+        else:
+            bot.send_message(chat_id=chat_id, text='Тест завершен, глянем на твои результаты.')
+            bot.send_sticker(chat_id=chat_id, data='CAACAgIAAxkBAAECMg5geq9IrghFjdKmFmEbjTigXmmGeAACMAADNraOCPwLBo-827_NHwQ')
+            results = sql.get_test_results(telegram_id, test_id)
+            correct = results[0]
+            incorrect = results[1]
+            num_questions = results[2]
+            text = f'Correct answers - {correct}\nIncorrect answers - {incorrect}\nNumber of questions - {num_questions}'
+            bot.send_message(chat_id=chat_id, text=text)
 
 
 if __name__ == '__main__':
